d06b1c5a676cb6eb7151fbd8edf1fdc65bf94e86
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-08-06 17:44:50 +0700

    Fix error of cannot convert from JSON string to JavaScript Object

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import usersRouter from './routes/users.routes'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// MEMO: Middleware for converting JSON string to JavaScript Object
+app.use(express.json())
+
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
