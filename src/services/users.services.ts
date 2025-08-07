a5ddf1f0077365dfcee19c93084f80acb944376b
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-08-07 13:03:38 +0700

    Encrypt password

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,10 +1,15 @@
 import User from '~/models/schemas/User.schema'
 import databaseService from './database.services'
 import { UserRegistrationRequestBody } from '~/models/schemas/requests/User.requests'
+import { hashPassword } from '~/utils/crypto'
 
 class UsersService {
   async register(payload: UserRegistrationRequestBody) {
-    const user = new User({ ...payload, date_of_birth: new Date(payload.date_of_birth) })
+    const user = new User({
+      ...payload,
+      password: hashPassword(payload.password),
+      date_of_birth: new Date(payload.date_of_birth)
+    })
     const result = await databaseService.users.insertOne(user)
     return result
   }
