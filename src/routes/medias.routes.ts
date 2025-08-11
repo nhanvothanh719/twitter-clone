ef8cc783794088ecf342bcd2ed3b78319b364603
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-08-11 15:34:46 +0700

    Add uploaded image validation

--- src/routes/medias.routes.ts
+++ src/routes/medias.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { uploadSingleImgController } from '~/controllers/medias.controllers'
+import { wrapRequestHandler } from '~/utils/handlers'
 
 const mediasRouter = Router()
 
-mediasRouter.post('/upload-image', uploadSingleImgController)
+mediasRouter.post('/upload-image', wrapRequestHandler(uploadSingleImgController))
 export default mediasRouter
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,3 +1,5 @@
+import { Request } from 'express'
+import formidable from 'formidable'
 import fs from 'fs'
 import path from 'path'
 import { UPLOAD_FOLDER_PATH } from '~/constants/paths'
@@ -9,4 +11,31 @@ export const initFolder = () => {
     fs.mkdirSync(UPLOAD_FOLDER_PATH, { recursive: true })
     console.log('--- Create `/uploads` folder successfully ---')
   }
+}
+
+export const handleSingleImgUpload = async (req: Request) => {
+  const maxFileSize = 300 * 1024 // MEMO: Max file size is 300KB
+  const form = formidable({
+    uploadDir: UPLOAD_FOLDER_PATH,
+    maxFiles: 1,
+    keepExtensions: true,
+    maxFileSize,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const isValidName = name === 'image' // MEMO: req.name === 'image'
+      const isValidMimeType = (mimetype as string).includes('image/')
+      const isValid = isValidName && isValidMimeType
+      if (!isValid) {
+        form.emit('error' as any, new Error('File type is invalid') as any)
+      }
+      return isValid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      // MEMO: If error occurs
+      if (err) return reject(err)
+      // MEMO: If successfully upload
+      resolve(files)
+    })
+  })
 }
